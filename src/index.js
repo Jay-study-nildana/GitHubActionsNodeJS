e57bcd445444fdbcdffe54e1e8f50ea8aedcdcfa
Jay <vijayasimhabr@live.com>
2023-03-23 21:47:47 +0530

    another change
    another change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-const express = require("express")
+// import {http} from 'http'
 
-const app = express()
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end('Hello World!');
+// }).listen(8080);
 
-app.get("/", (req, res) => {
-    res.send({
-        "first": "jason",
-        "last": "bourne"
-    })
-})
+import NodeServer from "./server/NodeServer"
+import ExpressServer from "./server/ExpressServer"
+import BootStrapServer from "./server/BootStrapServer"
+import dotenv from "dotenv"
+dotenv.config()
 
-const port = process.env.PORT;
+//use one of the two servers below.
 
-app.listen(port,()=> {
-    console.log("server running....")
-})
+// Start Node Server
+// const WebServer = new NodeServer(); //for basic website style
+
+//Start Express JS Server
+const expressServer = new ExpressServer(); //for api server style
+
+//start BootStrap site.
+//const bootStrapServer = new BootStrapServer();
